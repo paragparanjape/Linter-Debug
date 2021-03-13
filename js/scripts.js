a2b9646e7f737c6c4a3f85ab16defa2c7977f5e8
paragparanjape <78561372+paragparanjape@users.noreply.github.com>
2021-03-13 23:52:56 +0100

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+
+console.log('Hello! Welcome to my portfolio site!')
+
+function menuToggle () {
+  var x = document.getElementById('myNavtoggle')
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive'
+  } else {
+    x.className = 'navtoggle'
+  }
+}
